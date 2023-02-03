47aafa63ec0b0611d35c27d0b9f0173aaf03c78f
pkc533 <pkc533@gmail.com>
2023-02-03 19:32:50 +0100

    Update Paper_Script.R

--- Paper_Script.R
+++ Paper_Script.R
@@ -5,6 +5,7 @@ library("caretEnsemble")
 library("ggplot2")
 library("gridarrange")
 
+#Wrapper method(RFE) feature selection
 #Read the data
 train = read.csv("train_data.csv")
 test= read.csv("test_data.csv")
@@ -44,7 +45,7 @@ pred_MLR_RFE <- predict(MLR_RFE, RFE_te220)
 pred_SVR_RFE <- predict(SVR_RFE, RFE_te220)
 
 
-
+#Models with embedded feature selections
 ### Full dataset 
 
 allF_lasso <- train(tR~., data=train, method="glmnet",metric=metric,preProc=c("center","scale"),trControl=trainControl)
@@ -52,12 +53,12 @@ allF_rf <- train(tR~., data=train, method="rf", metric=metric,importance = T, pr
                  trControl=trainControl)
 allF_gbm <- train(tR~., data=train, method="gbm", metric=metric,preProc=c("center","scale"),
                   trControl=trainControl)
-#COmbining predictions
+#Combining predictions
 results <- resamples(list(mlr_cfs = MLR_CFS, svr_cfs = SVR_CFS, RFE_mlr = MLR_RFE, 
                               RFE_svr = SVR_RFE,lasso = allF_lasso, rf = allF_rf, gbm = allF_gbm))
 summary(results)
 
-#ENsemble model predictions
+#Ensemble model predictions
 pred_lasso <- predict(allF_lasso, test_zero220)
 pred_rf <- predict(allF_rf, test_zero220)
 pred_gbm <- predict(allF_gbm, test_zero220)
@@ -79,7 +80,7 @@ CVPred_train = cbind(train$tR, CVPred_MLR_CFS,CVPred_SVR_CFS,CVPred_MLR_RFE,CVPr
 df = read.csv("CV_pred_train.csv")
 test = read.csv("predictions_ext_Val.csv")
 
-############# Stacking Models #####################################################
+##### Stacking Models ######
 
 fit.stacknew <- train(tR~., data=df, method="lm", metric=metric,trControl=trainControl)
 
@@ -89,46 +90,6 @@ results_mods <- resamples(list(mlr_cfs = MLR_CFS, svr_cfs = SVR_CFS, RFE_mlr = M
 summary(results_mods)
 pred_stack <- predict(fit.stacknew, test[-1])
 
-## Saving model performances
-#MLR_CFS
-rmse_MLRCFS <- RMSE(pred_MLR_CFS, CFS_te220$tR)
-r2_MLRCFS <- R2(pred_MLR_CFS, CFS_te220$tR)
-#SVR_CFS
-rmse_SVRCFS <- RMSE(pred_SVR_CFS, CFS_te220$tR)
-r2_SVRCFS <- R2(pred_SVR_CFS, CFS_te220$tR)
-#MLR_RFE
-rmse_MLRRFE <- RMSE(pred_MLR_RFE, CFS_te220$tR)
-r2_MLRRFE <- R2(pred_MLR_RFEpred_MLR_CFS, CFS_te220$tR)
-#SVR_RFE               
-rmse_SVRRFE <- RMSE(pred_SVR_RFE, CFS_te220$tR)
-r2_SVRRFE <- R2(pred_SVR_RFE, CFS_te220$tR)
-#Lasso
-rmse_lasso <- RMSE(pred_lasso, CFS_te220$tR)
-r2_lasso<- R2(pred_lasso, CFS_te220$tR)
-#Random Forest
-rmse_rf <- RMSE(pred_rf, CFS_te220$tR)
-r2_rf <- R2(pred_rf, CFS_te220$tR)
-#GBM
-rmse_gbm <- RMSE(pred_gbm, CFS_te220$tR)
-r2_gbm <- R2(pred_gbm, CFS_te220$tR)
-#Stacking
-rmse_stack <- RMSE(pred_stcak, CFS_te220$tR)
-r2_stack <- R2(pred_stack, CFS_te220$tR)
-
-#Combining all performances
-Model_performances = cbind(rmse_MLRCFS,rmse_SVRCFS,
-                     rmse_MLRRFE,rmse_SVRRFE,
-                     rmse_lasso, rmse_rf,rmse_gbm,rmse_stack,
-                     r2_MLRCFS,r2_SVRCFS, 
-                     r2_MLRRFE,r2_SVRRFE,
-                     r2_lasso,r2_rf,r2_gbm,r2_stack,
-                     r2_MLRCFS,r2_SVRCFS,
-                     r2_MLRRFE, r2_SVRRFE,
-                     r2_lasso,r2_rf,r2_gbm,r2_stack)
-
-
-
-write.csv(Model_performances,"model_performances.csv")
 
 #Prediced vs. Experimental retention times plots
 obs=df$tR
